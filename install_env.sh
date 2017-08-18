19b20f6aa7f60cd899e4b1cf9c1dca43f9da1052
tww <1906864691@qq.com>
2017-08-18 15:03:29 +0800

    localhost3

--- install_env.sh
+++ install_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Install everything
-sudo apt-get install lamp-server^
+#sudo apt-get install lamp-server^
+sudo apt-get install apache2
 sudo ls /var/www/
 sudo service apache2 restart
